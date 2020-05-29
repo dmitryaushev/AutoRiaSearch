f7bdd4def4c92cb5b267d795a0840ee2f264b9c2
Aushev <dmitryaushev@gmail.com>
2020-05-29 14:34:20 +0300

    init commit

---
+++ src/main/java/com/aushev/autoriasearch/AutoRiaSearchApplication.java
@@ -0,0 +1,13 @@
+package com.aushev.autoriasearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoRiaSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutoRiaSearchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/aushev/autoriasearch/AutoRiaSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aushev.autoriasearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoRiaSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
